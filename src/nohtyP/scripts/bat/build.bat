b853c07d10a29fbdeae936563694c37f608b167a
DVP-F <carnx@duck.com>
2026-06-04 21:56:21 +0100

    shuffle

--- src/nohtyP/scripts/bat/build.bat
+++ src/nohtyP/scripts/bat/build.bat
@@ -1,21 +1,32 @@
+:: no output by defult
 @echo off
+:: enable vars expandable after execution
 setlocal enabledelayedexpansion
-chcp 65001 >nul 
+:: UTF-8 codepage
+chcp 65001 >nul
 
-goto :start
+call :start
+set "RC=%ERRORLEVEL%"
+call :clean
+exit /b %RC%
 
-:clean
-del /r /f LICENSES\
-rmdir LICENSES
+:start
+set "STARTDIR=%cd%"
+(
+    :: do stuff
+) || rem no
 goto :eof
 
 :copy_files
-copy ...
-exit /b
-
-:start
-set STARTDIR="%cd%"
 (
-
+    copy ...
 )
-call :clean
+goto :eof
+
+:clean
+(
+    rmdir /s /q LICENSES
+    cd !STARTDIR!
+) || rem no
+:: Ignore errors, exit immediately after anyways
+goto :eof
