b98dadf23ed692d2a721469061a635530cc9dc49
DVP-F <carnx@duck.com>
2026-06-04 12:33:05 +0200

    add refs

--- src/nohtyP/__init__.py
+++ src/nohtyP/__init__.py
@@ -1,2 +1,6 @@
 #? init and entry point of the package
+# https://docs.python.org/3/tutorial/modules.html#importing-from-a-package 
+# https://docs.python.org/3/library/distribution.html 
+# https://docs.python.org/3/library/__main__.html#main-py-in-python-packages 
+
 
