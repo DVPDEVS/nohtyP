6a201bb963ec54ca94493e0adea2a87c405c94f1
DVP-F <carnx@duck.com>
2026-06-10 08:29:45 +0200

    uhh

--- src/nohtyP/lexer/identifier.py
+++ src/nohtyP/lexer/identifier.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from nohtyP.global_utilities.decorators import *
 from nohtyP.lexer.types import *
-# from nohtyP.lexer.tt import TT, PY
+from nohtyP.lexer.tt import *
 import re
 # Identify objects
 
@@ -11,20 +11,15 @@ class Identify:
 	"""Identify lexical objects"""
 	def single_element(element :str) -> LexObject:
 		"""Try to identify a single lexical object in a `str` container"""
-		for key, (reg, object) in TT.YP.items():
+		for _key, (reg, object) in TT.YP.items():
 			if re.match(reg, element):
 				return LexObject(element, object)
-		for key, regex in PY.REGEX:
-			if re.match(regex, element):
-				return LexObject(element, PY.ELEM[key])
+		for _key, (reg, object) in TT.PY.items():
+			if re.match(reg, element):
+				return LexObject(element, object)
 		return LexObject(element, ...)
 	def series(elements :TokenSeries) -> LexObjectSeries:
 		result = LexObjectSeries()
 		for i in elements:
 			result.append(Identify.single_element(i))
 		return result
-
-from typing import reveal_type
-
-reveal_type(TT)
-reveal_type(TT.G)
