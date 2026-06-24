4a80bdcfa91ffc7892ab4be46eeaa2b190b2aa05
DVP-F <carnx@duck.com>
2026-06-24 21:39:15 +0100

    add echos and fix script

--- src/nohtyP/_dev/scripts/sh/build.sh
+++ src/nohtyP/_dev/scripts/sh/build.sh
@@ -25,7 +25,7 @@ ln -sfn ../LICENSES LICENSES
 ## clean out dist
 rm -rf ./dist/
 ## clean out pycache ( rember ´chmod +x clean_cache.sh´)
-./nohtyP/_dev/scripts/clean_cache.sh
+./nohtyP/_dev/scripts/sh/clean_cache.sh || true
 
 # create temporary venv
 TEMP_VENV=$(mktemp -d)
@@ -43,18 +43,23 @@ _YP_HATCH_BUILD_MODE=release hatch build --target wheel
 _YP_HATCH_BUILD_MODE=sdist   hatch build --target sdist
 _YP_HATCH_BUILD_MODE=dev     hatch build --target wheel
 
-# find wheel and tarball
+# find wheels and tarball
 shopt -s nullglob
 ## dev wheels
-dwhls=(dist/nohtyP*dev*.whl)
+dwhls=(dist/nohtyp*dev*.whl)
 ## release wheels
-rwhls=(dist/nohtyP*.whl) ; tmp=()
+rwhls=(dist/nohtyp*.whl) ; tmp=()
 for f in "${rwhls[@]}"; do
     [[ $f == *dev* ]] || tmp+=("$f")
 done
 rwhls=("${tmp[@]}")
 ## tarballs
-tars=(dist/nohtyP*.tar.gz)
+tars=(dist/nohtyp*.tar.gz)
+# report count
+echo
+echo Release wheels found: ${#rwhls[@]}
+echo Dev wheels found:     ${#dwhls[@]}
+echo Tarballs found:       ${#tars[@]}
 # ensure non-zero count
 if [[ ${#rwhls[@]} -eq 0 || ${#dwhls[@]} -eq 0 || ${#tars[@]} -eq 0 ]]; then
     echo "Build failed: artifacts not found"
@@ -66,18 +71,30 @@ latest_dev=${dwhls[0]}
 latest_tar=${tars[0]}
 
 # inspect wheels and tarball
+echo
+echo === Inspecting release wheel ===
 unzip -l "$latest_rel"
+echo
+echo ===== Inspecting dev wheel =====
 unzip -l "$latest_dev"
+echo
+echo === Inspecting sdist tarball ===
 tar -tf "$latest_tar"
 
 # TODO: add smoke test
 
 # test install
+echo
+echo Testing release install
 python -m pip install --no-cache-dir "$latest_rel"
+echo Executing...
 python -m nohtyP
 
 # test dev extra
+echo
+echo Testing dev install
 python -m pip uninstall -y nohtyP
 python -m pip install --no-cache-dir "$latest_dev"
+echo Executing...
 python -m nohtyP
 
