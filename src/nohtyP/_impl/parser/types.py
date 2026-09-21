2238ab0fda3cc79fec715f2fe490ce53e84b32ba
DVP-F <carnx@duck.com>
2026-09-21 10:51:23 +0200

    various improvements and vsc is a dickhead

--- src/nohtyP/_impl/parser/types.py
+++ src/nohtyP/_impl/parser/types.py
@@ -12,8 +12,7 @@ class SyntaxObject:
 	def __init__(self, kind :str) -> None:
 		self._kind :str = kind
 	def __repr__(self) -> str:
-		# any instance of SyntaxObject in structure will have their __str__ dunder called here, not __repr__
-		return f"SyntaxObject(kind:'{self._kind}', structure: {self._struct})"
+		return f"SyntaxObject(kind:'{self._kind}')"
 	def __str__(self) -> str:
 		return self._kind
 
@@ -86,6 +85,13 @@ class ParseObject:
 		self._sotype :SyntaxObjectList|None = None # NOP string
 		self.objectlist :tuple[LexObject] = () # any non-zero amount
 		self.__issue_list__ :tuple[str|AnyNohtyPSyntaxError] = ()
+	# modify
+	## objects
+	def __iadd__(self, obj:LexObject) -> None: self.objectlist += obj
+	def add_object(self, obj:LexObject) -> None: self += obj       # pass to the above dunder
+	## type
+	def __ilshift__(self, obj: SyntaxObjectList) -> None: self._sotype = obj
+	def add_type(self, obj:SyntaxObjectList) -> None: self <<= obj # pass to the above dunder
 	# strings
 	def __str__(self) -> str: return f"{self._sotype}[{self.objectlist}]"
 	def __repr__(self) -> str:
@@ -112,32 +118,32 @@ class ParseObjectSeries:
 	NohtyP class for holding a series of `ParseObject`
 	"""
 	# TODO: update dunders below to reflect actual new data
-	__slots__ = ["tokenlist",]
+	__slots__ = ["po_list",]
 	def __init__(self):
-		self.tokenlist :tuple[ParseObject] = []
+		self.po_list :tuple[ParseObject] = []
 		pass
 	# object handling
-	def append(self, obj :ParseObject) -> None: self.tokenlist.append(obj)
+	def append(self, obj :ParseObject) -> None: self.po_list.append(obj)
 	# strings
 	def __str__(self) -> None:
 		string = ""
-		for i in range(len(self.tokenlist)):
-			string += f"  {i}:\t{self.tokenlist[i]}\n"
+		for i in range(len(self.po_list)):
+			string += f"  {i}:\t{self.po_list[i]}\n"
 		return string[:-1]
 	def __repr__(self) -> None:
 		string = "LexObjectSeries:\n"
-		for i in range(len(self.tokenlist)):
-			string += f" {i}:\t{self.tokenlist[i].__repr__()}\n"
+		for i in range(len(self.po_list)):
+			string += f" {i}:\t{self.po_list[i].__repr__()}\n"
 		return string[0:-1]
 	# iteration support
-	def __getitem__(self, key:int): return self.tokenlist[key] # pass on to a tuple
-	def __iter__(self): yield from self.tokenlist # pass on to a tuple
+	def __getitem__(self, key:int): return self.po_list[key] # pass on to a tuple
+	def __iter__(self): yield from self.po_list # pass on to a tuple
 	# added for testing, might be used more
 	def format_list(self, include_position: bool = False) -> list[list[str, str, str]] | list[tuple[str, str, str, int]]:
 		return [
 			[ str(object._sotype._lang), object._sotype._name, object.value() ]
-			for object in self.tokenlist
+			for object in self.po_list
 		] if not include_position else [
 			( str(object._sotype._lang), object._sotype._name, object.value(), object.position() )
-			for object in self.tokenlist
+			for object in self.po_list
 		]
